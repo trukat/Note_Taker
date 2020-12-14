89619f8bffa96d71075d235300da527152806cdf
TruKat <kati.k220@gmail.com>
2020-12-13 19:39:41 -0700

    Initial commit

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = (app) => {
+  // => HTML GET Requests
+  // In each of the below cases the user is shown an HTML page of content
+  app.get("/api/notes", (req, res) => {
+
+  });
+
+  app.post("/api/notes", (req, res) => {
+      
+  });
+};
