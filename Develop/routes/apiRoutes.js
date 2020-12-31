6238071a0d5a2d21c54949236fbf8899260d3635
TruKat <kati.k220@gmail.com>
2020-12-30 19:45:18 -0700

    UPDATE: id creating random strings

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -3,12 +3,15 @@ const {nanoid} = require("nanoid")
 const fs = require('fs')
 const path = require("path");
 const db = path.join(__dirname, "../db/db.json");
+const { v4: uuidv4 } = require('uuid');
 
 // /api/notes
 router.get("/notes", (req, res) => {
     //console.log(db);
     fs.readFile(db, "utf8", (err, data) => {
-      if (err) throw err;
+      if (err) {
+          let parsedNotes = [];
+      };
       //console.log(data);
       return res.json(JSON.parse(data));
    
@@ -16,20 +19,29 @@ router.get("/notes", (req, res) => {
   });
 
 router.post("/notes", (req, res) => {
-    console.log(req.body)
-    fs.readFile(db, "utf8", (err, data) => {
-        if (err) throw (err);
-
-        const allNotes = JSON.parse(data);
-
+    let allNotes = [];
         let newNote = {
-            id: 1,
+            id: uuidv4(),
             title: req.body.title,
             text: req.body.text, 
         }
 
         allNotes.push(newNote);
         fs.writeFile(db, JSON.stringify(allNotes),(err) => {
+            if (err) throw (err);
+            res.json({msg: "success"})
+        });
+    });
+
+router.delete("/notes/:id", (req, res) =>{
+    fs.readFile(db, "utf8", (err, data) => {
+        if (err) throw (err);
+        let id = req.params.id;
+        console.log(id);
+        const allNotes = JSON.parse(data);
+        console.log(allNotes);
+        const remainingNotes = allNotes.filter(note => note.id !== id);
+        fs.writeFile(db, JSON.stringify(remainingNotes),(err) => {
             if (err) throw (err);
             res.json({msg: "success"})
         });
