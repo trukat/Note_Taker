de23fbd442488d7ff80fe4745b24bcb006bb5cfc
TruKat <kati.k220@gmail.com>
2020-12-30 23:20:59 -0700

    UPDATE: delete method functional

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -9,43 +9,43 @@ const { v4: uuidv4 } = require('uuid');
 router.get("/notes", (req, res) => {
     //console.log(db);
     fs.readFile(db, "utf8", (err, data) => {
-      if (err) {
-          let parsedNotes = [];
-      };
-      //console.log(data);
-      return res.json(JSON.parse(data));
-   
+        if (err) throw err;
+        //console.log(data);
+        return res.json(JSON.parse(data));
+      });
     });
-  });
 
-router.post("/notes", (req, res) => {
-    let allNotes = [];
-        let newNote = {
-            id: uuidv4(),
-            title: req.body.title,
-            text: req.body.text, 
-        }
-
-        allNotes.push(newNote);
-        fs.writeFile(db, JSON.stringify(allNotes),(err) => {
+    router.post("/notes", (req, res) => {
+        fs.readFile(db, "utf8", (err, data) => {
             if (err) throw (err);
-            res.json({msg: "success"})
+            const allNotes = JSON.parse(data);
+            let newNote = {
+                id: uuidv4(),
+                title: req.body.title,
+                text: req.body.text, 
+            }
+            allNotes.push(newNote);
+            fs.writeFile(db, JSON.stringify(allNotes),(err) => {
+                if (err) throw (err);
+                res.json({msg: "success"})
+            });
         });
     });
 
-router.delete("/notes/:id", (req, res) =>{
-    fs.readFile(db, "utf8", (err, data) => {
-        if (err) throw (err);
-        let id = req.params.id;
-        console.log(id);
-        const allNotes = JSON.parse(data);
-        console.log(allNotes);
-        const remainingNotes = allNotes.filter(note => note.id !== id);
-        fs.writeFile(db, JSON.stringify(remainingNotes),(err) => {
+    router.delete("/notes/:id", (req, res) =>{
+        fs.readFile(db, "utf8", (err, data) => {
             if (err) throw (err);
-            res.json({msg: "success"})
+            let id = req.params.id;
+            console.log(id);
+            const allNotes = JSON.parse(data);
+            console.log(allNotes);
+            const remainingNotes = allNotes.filter(note => note.id !== id);
+            fs.writeFile(db, JSON.stringify(remainingNotes),(err) => {
+                if (err) throw (err);
+                res.json({msg: "success"})
+            });
         });
     });
-});
+    
 
 module.exports = router;
